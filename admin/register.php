ae7f4ac42214a2aa529605b3c6047a351eb48972
chenwenxuan <1763516926@qq.com>
2023-04-02 10:18:37 +0800

    try netlify for php

---
+++ admin/register.php
@@ -0,0 +1,23 @@
+<?php
+include 'common.php';
+if ($user->hasLogin() || !$options->allowRegister) {
+    $response->redirect($options->siteUrl);
+}
+$sys_protocal = isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == '443' ? 'https://' : 'http://';
+$path_info = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '';
+$relate_url = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : $php_self.(isset($_SERVER['QUERY_STRING']) ? '?'.$_SERVER['QUERY_STRING'] : $path_info);
+$url = urlencode($sys_protocal.(isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : '').$relate_url);
+$url = Typecho_Common::url('user/register', Helper::options()->index).'?from='.$url;
+?>
+
+<HTML>
+
+<head>
+
+    <meta HTTP-EQUIV="REFRESH" CONTENT="3; URL='<?php echo $url; ?>' ">
+
+</head>
+
+<body>
+
+</body>
